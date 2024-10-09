5b95722c2bc874c337fb2003bac2397b3d86a288
Premchand Jalla <premchandjalla777@gmail.com>
2024-10-09 16:39:29 -0400

    Update launch_app.py

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def launch_app():
-    subprocess.run(["python3", "app/main.py"])
+    subprocess.run(["python3", "app/app.py"])
 
 if __name__ == "__main__":
     launch_app()
